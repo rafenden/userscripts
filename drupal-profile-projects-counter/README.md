601f8fa95c3d2d1ec43a663df174aeb974e87d03
Rafal Enden <git@enden.com>
2021-11-12 12:58:11 +0000

    Add drupal-profile-projects-counter

---
+++ drupal-profile-projects-counter/README.md
@@ -0,0 +1,5 @@
+# drupal.org — Number of total projects
+
+ Show number of projects on the drupal.org user profile.
+
+![Screenshot](screenshot.png)
---
+++ drupal-profile-projects-counter/index.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         drupal.org — Number of total projects
+// @description  Show number of projects on the drupal.org user profile.
+// @author       Rafal Enden
+// @namespace    https://github.com/rafenden
+// @homepageURL  https://github.com/rafenden/userscripts/drupal-profile-projects-counter
+// @supportURL   https://github.com/rafenden/userscripts/issues
+// @version      1.0
+// @match        *://www.drupal.org/u/*
+// @grant        none
+// ==/UserScript==
+
+'use strict'
+
+const projectsElement = document.querySelector('.versioncontrol-project-user-commits')
+const totalProjects = projectsElement.querySelectorAll('li').length
+projectsElement.insertAdjacentHTML('beforebegin', `<p>Total projects: ${totalProjects}</p>`)
